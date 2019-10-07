21bf96f356823f62d3e5ff4ab53b5d05d80acc8f
jenipha <jenipharachel@gmail.com>
2019-10-07 22:57:31 +0530

    day 1 work

---
+++ Day_1/test1.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+http
+  .createServer(function(req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+      if (filename == "./") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h1>Welcome to my training site</h1>");
+        return res.end();
+      }
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8084);
+
+console.log("Hello");
