a115f7f2aa4d87335e660da3ad9dbe2ee0dccf0a
Jenipha Rachel <jenipharachel@gmail.com>
2019-10-22 21:49:20 +0530

    task completed but check version of jquery for using left

--- Day_5/WeekendTask/custom.js
+++ Day_5/WeekendTask/custom.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
   setTimeout(function() {
     $("body").css({ "background-color": "grey" });
   }, 5000);
+
   $("#myname")
     .delay(10000)
     .animate({ "margin-left": "500px" });
+
   $("#tabledata")
     .delay(15000)
     .animate({ "margin-left": "450px" });
+
   $("#showhide").on("change", function() {
     if (this.value == "show") {
       $("#tables").show();
